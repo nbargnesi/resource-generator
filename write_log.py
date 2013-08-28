6576a34a33a9a0dd5bdb67bc3fda9efc78ec26f6
jhourani <jordanhourani@gmail.com>
2013-08-28 13:43:25 -0400

    Fixed the change_log output.

--- write_log.py
+++ write_log.py
@@ -7,5 +7,4 @@ import json
 def write(change_log):
 
     with open('change_log.txt', 'w') as fp:
-        for entry in change_log:
-            json.dump(entry, fp, sort_keys=True, indent=2, separators=(', ', ':'))
+        json.dump(change_log, fp, sort_keys=True, indent=4, separators=(', ', ':'))

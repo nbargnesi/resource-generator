5304ab7f4fa54292cb5486b38bdc642f079bc313
Natalie Catlett <ncatlett@selventa.com>
2014-02-06 00:47:40 -0500

    added string-matching of alt and pref labels to create closeMatch edges update to allow multiple uses of -d command line argument

--- rdf.py
+++ rdf.py
@@ -3,14 +3,10 @@
 
 import argparse
 import os
-import parsed
 import pickle
 import time
-import shutil
-import annotate
-from common import download
-from constants import PARSER_TYPE, RES_LOCATION
 import datasets
+from collections import defaultdict
 from rdflib import URIRef, BNode, Literal, Namespace, Graph
 from rdflib.namespace import RDF, RDFS, SKOS, DCTERMS, OWL, XSD
 from urllib import parse
@@ -22,20 +18,20 @@ belv = Namespace("http://www.openbel.org/vocabulary/")
 parser = argparse.ArgumentParser(description="""Generate namespace and equivalence files
 for gene/protein datasets.""")
 
-parser.add_argument("-n", required=True, nargs=1, metavar="DIRECTORY",
+parser.add_argument("-n", required=True, metavar="DIRECTORY",
 					help="directory to store the new namespace equivalence data")
-parser.add_argument("-d", required=False, help="dataset by prefix; if none specified, all datasets in directory will be run")
+parser.add_argument("-d", required=False, action='append', help="dataset by prefix; if none specified, all datasets in directory will be run")
 args = parser.parse_args()
-
-if os.path.exists(args.n[0]):
-    os.chdir(args.n[0])
+if os.path.exists(args.n):
+    os.chdir(args.n)
 else:
-    print('data directory {0} not found!'.format(args.n[0]))
+    print('data directory {0} not found!'.format(args.n))
 
 
 # loads parsed data from pickle file (after running phase 2 of gp_baseline.py)
 def make_rdf(d, g):
-	# build RDF and serialize
+	''' Given namepsace data object d and graph g,
+	adds to  namespace rdf graph. ''' 
 	# make namespace for data set (using data object attributes '_name' and '_prefix')
 	n = Namespace("http://www.openbel.org/bel/namespace/" + d._name + '/')
 
@@ -116,27 +112,70 @@ def make_rdf(d, g):
 						xref_uri = URIRef(xrefns[value])
 						g.add((term_uri, SKOS.exactMatch, xref_uri))
 
-	
-g = Graph()
+def get_close_matches(concept_type, g):
+	''' Given namespace concept type and graph, g, 
+	searches graph for items of the given concept type and 
+	identifies close matches based on case-insensitive string matching 
+	of synonyms. Adds skos:closeMatch edges to graph. '''
 
-# build dictionary of namespace prefixes to names (from dataset objects)
-print('gathering namespace information ...')
-prefix_dict = {}
-for files in os.listdir("."):
-	if files.endswith("parsed_data.pickle"):
-		with open(files, 'rb') as f:
-			d = pickle.load(f)
-		if isinstance(d, datasets.NamespaceDataSet):
-			prefix_dict[d._prefix] = d._name
-			print('\t{0} - {1}'.format(d._prefix, d._name))
+	print('gathering string matches for {0}s ...'.format(concept_type))
+	concept_dict = defaultdict(dict)
+	for c in g.subjects(RDF.type, belv[concept_type]):
+		labels = list(g.objects(c, SKOS["altLabel"]))
+		labels.extend(g.objects(c, SKOS["prefLabel"]))
+		labels = [l.casefold() for l in labels]
+		scheme =  set(g.objects(c, SKOS["inScheme"])).pop()
+		concept_dict[scheme][c] = set(labels)
+
+#	concepts = list(sorted(concept_dict.keys()))
+#	count = 0
+#	while len(concepts) > 0:
+#		concept = concepts.pop()
+#		synonyms = concept_dict.pop(concept)
+#		for concept2, synonyms2 in concept_dict.items():
+#			if len(synonyms.intersection(synonyms2)) > 0:
+#				g.add((concept, SKOS.closeMatch, concept2))
+#				count += 1
+	count = 0
+	for scheme, concept_map in concept_dict.items():
+		for concept, labels in concept_map.items():
+			# iterate concept_dict a 2nd time, skipping items in same scheme
+			for scheme2 in concept_dict.keys():
+				if scheme2 == scheme:
+					continue
+				else:
+					for concept2, labels2 in concept_dict[scheme2].items():
+					#for concept2, labels2 in concept_map2.items():
+						if len(labels.intersection(labels2)) > 0:
+							g.add((concept, SKOS.closeMatch, concept2))
+							count += 1 
+	print('\tadded {0} closeMatches for {1}s'.format(count, concept_type))
+
+if __name__=='__main__':	
+	g = Graph()
+
+	# build dictionary of namespace prefixes to names (from dataset objects)
+	print('gathering namespace information ...')
+	prefix_dict = {}
+	for files in os.listdir("."):
+		if files.endswith("parsed_data.pickle"):
+			with open(files, 'rb') as f:
+				d = pickle.load(f)
+			if isinstance(d, datasets.NamespaceDataSet):
+				prefix_dict[d._prefix] = d._name
+				print('\t{0} - {1}'.format(d._prefix, d._name))
 	
-for files in os.listdir("."):
-	if files.endswith("parsed_data.pickle"):
-		with open(files,'rb') as f:
-			d = pickle.load(f)
-			if isinstance(d, datasets.NamespaceDataSet) and args.d == None:
-				make_rdf(d, g)
-			elif isinstance(d, datasets.NamespaceDataSet) and d._prefix in args.d:
-				make_rdf(d, g)
-print('serializing RDF graph ...')
-g.serialize("testfile.ttl", format='turtle')	
+	for files in os.listdir("."):
+		if files.endswith("parsed_data.pickle"):
+			with open(files,'rb') as f:
+				d = pickle.load(f)
+				if isinstance(d, datasets.NamespaceDataSet) and args.d == None:
+					make_rdf(d, g)
+				elif isinstance(d, datasets.NamespaceDataSet) and d._prefix in args.d:
+					make_rdf(d, g)
+
+	get_close_matches('BiologicalProcessConcept', g)
+	get_close_matches('AbundanceConcept', g)
+	
+	print('serializing RDF graph ...')
+	g.serialize("testfile.ttl", format='turtle')	

db79fee336563ea9c20c65fcd87920e6dff019a9
Natalie Catlett <ncatlett@selventa.com>
2014-03-20 13:37:08 -0400

    added queries for preflabel and ID conflicts

--- sparql_test.py
+++ sparql_test.py
@@ -7,45 +7,74 @@ from SPARQLWrapper import SPARQLWrapper, GET, JSON
 
 
 def query(query, endpoint):
-    sparql = SPARQLWrapper(endpoint)
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    sparql.setMethod(GET)
+	sparql = SPARQLWrapper(endpoint)
+	sparql.setQuery(query)
+	sparql.setReturnFormat(JSON)
+	sparql.setMethod(GET)
 
-    try:
-        result = sparql.query()
-        return result.convert()
-    except HTTPError as e:
-        print(str(e))
+	try:
+		result = sparql.query()
+		return result.convert()
+	except HTTPError as e:
+		print(str(e))
 
 if __name__ == '__main__':
-    import sys
+	import sys
 
-    if len(sys.argv) != 2:
-        sys.stderr.write('usage: sparql_test.py [SPARQL_ENDPOINT_URL]\n')
-        sys.exit(1)
+	if len(sys.argv) != 2:
+		sys.stderr.write('usage: sparql_test.py [SPARQL_ENDPOINT_URL]\n')
+		sys.exit(1)
 
-    endpoint_url = sys.argv[1]
-    EXACT_MATCH_ORPHANS = """
-    prefix skos: <http://www.w3.org/2004/02/skos/core#>
-    select (count(distinct ?uri2) as ?count)
-    where {
-      ?uri1 skos:exactMatch ?uri2 .
-      minus { ?uri2 skos:inScheme ?scheme .}
-    }
-    """
-    ORTHOLOGOUS_MATCH_ORPHANS = """
-    prefix skos: <http://www.w3.org/2004/02/skos/core#>
-    prefix belv: <http://www.openbel.org/vocabulary/>
-    select (count(distinct ?uri2) as ?count)
-    where {
-      ?uri1 belv:orthologousMatch ?uri2 .
-      minus { ?uri2 skos:inScheme ?scheme .}
-    }
-    """
+	endpoint_url = sys.argv[1]
+	EXACT_MATCH_ORPHANS = """
+	prefix skos: <http://www.w3.org/2004/02/skos/core#>
+	select (count(distinct ?uri2) as ?count)
+	where {
+	  ?uri1 skos:exactMatch ?uri2 .
+	  minus { ?uri2 skos:inScheme ?scheme .}
+	}
+	"""
+	ORTHOLOGOUS_MATCH_ORPHANS = """
+	prefix skos: <http://www.w3.org/2004/02/skos/core#>
+	prefix belv: <http://www.openbel.org/vocabulary/>
+	select (count(distinct ?uri2) as ?count)
+	where {
+	  ?uri1 belv:orthologousMatch ?uri2 .
+	  minus { ?uri2 skos:inScheme ?scheme .}
+	}
+	"""
+	ID_CONFLICTS = """
+	prefix skos: <http://www.w3.org/2004/02/skos/core#>
+	prefix dc: <http://purl.org/dc/terms/>
+	select (count(distinct ?uri2) as ?count)
+	where {
+	?uri1 dc:identifier ?id1 .
+	?uri2 dc:identifier ?id1 .
+	?uri1 skos:inScheme ?scheme .
+	?uri2 skos:inScheme ?scheme .
+	FILTER (?uri1 != ?uri2) .
+	}
+	""" 	
+	PREFLABEL_CONFLICTS = """
+	prefix skos: <http://www.w3.org/2004/02/skos/core#>
+	prefix dc: <http://purl.org/dc/terms/>
+	select(count(distinct ?uri2) as ?count)
+	where {
+	?uri1 skos:prefLabel ?label .
+	?uri2 skos:prefLabel ?label .
+	?uri1 skos:inScheme ?scheme .
+	?uri2 skos:inScheme ?scheme .
+	FILTER (?uri1 != ?uri2) .
+	}
+	"""	 
+	print("Orphans for exactMatch:")
+	print(dumps(query(EXACT_MATCH_ORPHANS, endpoint_url), indent=4))
 
-    print("Orphans for exactMatch:")
-    print(dumps(query(EXACT_MATCH_ORPHANS, endpoint_url), indent=4))
+	print("Orphans for orthologousMatch:")
+	print(dumps(query(ORTHOLOGOUS_MATCH_ORPHANS, endpoint_url), indent=4))
 
-    print("Orphans for orthologousMatch:")
-    print(dumps(query(ORTHOLOGOUS_MATCH_ORPHANS, endpoint_url), indent=4))
+	print("ID conflicts:")
+	print(dumps(query(ID_CONFLICTS, endpoint_url), indent=4))
+
+	print("prefLabel conflicts:")
+	print(dumps(query(PREFLABEL_CONFLICTS, endpoint_url), indent=4))

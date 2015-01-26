1d55da9a7bf98b5a7cca71892e10e37e29e3a3ec
Natalie Catlett <ncatlett@selventa.com>
2015-01-26 11:13:03 -0500

    updated meshc to include supplemental concepts w/o semantic types many chemical terms do not have associated types

--- parsed.py
+++ parsed.py
@@ -246,15 +246,15 @@ def build_data(entry, parser, data_object):
 					'sts' : sts,
 					'mns' : mns,
 					'synonyms' : synonyms }
-		if any(branch.startswith('D') for branch in mns) or len(mns) == 0:
-			# filter by semantic type for chemicals - 
+		if any(branch.startswith('D') for branch in mns) or ui.startswith('C'):
+			# filter by semantic type for chemicals, but allow all non-typed terms in 
 			# see http://semanticnetwork.nlm.nih.gov/SemGroups/SemGroups.txt
 			chemicals = ('T116','T195','T123','T122','T118','T103','T120',
 				'T104','T200','T111','T196','T126','T131','T125','T129','T130',
 				'T197','T119','T124','T114','T109','T115','T121','T192','T110',
 				'T127')
 
-			if any(st in chemicals for st in sts):
+			if any(st in chemicals for st in sts) or len(sts) == 0:
 				meshc_dict[ui] = {
 					'mesh_header' : mh,
 					'sts' : sts,

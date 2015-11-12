0f077a450adfb468d7a4e9d27e31d8000d2ab2dd
Nick Bargnesi <nbargnesi@selventa.com>
2015-11-12 09:18:30 -0500

    add run script; to generate namespaces

---
+++ java/scripts/05-run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# The next three lines are for the go shell.
+export SCRIPT_NAME="run"
+export SCRIPT_HELP="Invokes buildr run."
+[[ "$GOGO_GOSH_SOURCE" -eq 1 ]] && return 0
+
+# Normal script execution starts here.
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
+. "$DIR"/env.sh || exit 1
+assert-env-or-die RG_JAVA_SCRIPTS
+"$RG_JAVA_SCRIPTS"/run.sh

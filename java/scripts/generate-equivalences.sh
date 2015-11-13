3c500a7fc5cd4401cd22ebc971da220e5466421e
Nick Bargnesi <nbargnesi@selventa.com>
2015-11-13 12:35:10 -0500

    add launcher scripts

---
+++ java/scripts/generate-equivalences.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
+. "$DIR"/env.sh || exit 1
+assert-env-or-die RG_JAVA
+cd "${RG_JAVA}" || exit 1
+jars=$(find lib -name "*.jar" -printf "%p:")
+java -Dorg.apache.logging.log4j.simplelog.StatusLogger.level=OFF \
+    -cp build:$jars org.openbel.reggie.rdf.generate.equivalences.Main
+
